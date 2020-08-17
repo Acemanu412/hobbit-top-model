aa1fc64d0a781905a86d82f2eb5fc4a97facb1c3
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-17 16:03:32 -0700

    Initial commit'

---
+++ test/shire_top_model/core_test.clj
@@ -0,0 +1,7 @@
+(ns shire-top-model.core-test
+  (:require [clojure.test :refer :all]
+            [shire-top-model.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
