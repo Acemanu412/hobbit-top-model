faccbbc6340ac189afce6b72681e5fd861c9cd1f
Manu Aceituno <manuel.n.aceituno@gmail.com>
2020-08-17 17:59:58 -0700

    Solved bugs regarding parentheses placement

--- src/shire_top_model/core.clj
+++ src/shire_top_model/core.clj
@@ -26,20 +26,29 @@
   (defn matching-part
     [part]
     {:name (clojure.string/replace (:name part) #"^left-" "right-")
-     :size (:size part)})
+    :size (:size part)})
 
   (defn symmetrize-body-parts
     "Expects a seq of maps that have a :name and :size"
     [asym-body-parts]
     (loop [remaining-asym-parts asym-body-parts
            final-body-parts []]
-      if (empty? remaining-asym-parts)
-      final-body-parts
-      (let [[part & remaining] remaining-asym-parts]
-        (recur remaining
-               (into final-body-parts
-                     (set [part (matching-part part)]))))))
-  (symmetrize-body-parts asym-hobbit-body-parts)
-  )
+      (if (empty? remaining-asym-parts)
+        final-body-parts
+        (let [[part & remaining] remaining-asym-parts]
+          (recur remaining
+                 (into final-body-parts
+                       (set [part (matching-part part)])))))))
+  
+  (defn better-symmetrize-body-parts
+    "Expects a seq of maps that have a :name and :size"
+    [asym-body-parts]
+    (reduce (fn [final-body-parts part]
+              (into final-body-parts (set [part (matching-part part)])))
+            []
+            asym-body-parts))
+  
+  (better-symmetrize-body-parts asym-hobbit-body-parts))
+  
 
     
